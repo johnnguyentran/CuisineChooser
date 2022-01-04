57d73bde7e35b2a84df85e18d226eefdf19ad78e
John Nguyen-Tran <john.n.nguyentran@gmail.com>
2022-01-03 20:45:45 -0500

    Expanded seekbar and spinners.

--- app/src/main/java/com/johnnguyentran/cuisinechooser/MainActivity.java
+++ app/src/main/java/com/johnnguyentran/cuisinechooser/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,34 +16,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSpinnerFoodType();
-    }
-
-    private void setSpinnerFoodType(){
         // Get the spinner from the .xml
         Spinner spinnerFoodType = (Spinner)findViewById(R.id.spinnerFoodTypeDropdown);
-
         // Create the list of items
         String[] items = new String[]{"No preference", "Chinese", "Mexican", "Italian",
                 "Japanese", "Thai", "Indian", "Mediterranean"};
-
         // Create an adapter to display items
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-
         // Set spinner from .xml to the created adapter
         spinnerFoodType.setAdapter(adapter);
 
-    }
 
-    private void setSeekBarPricing(){
         // Get the SeekBar from the .xml
         SeekBar seekBarPricing = (SeekBar)findViewById(R.id.seekBarPricing);
-        
+        TextView textSeekBarPricing = (TextView)findViewById(R.id.textSeekBarPricing);
         // Set maximum of 5 options ($ to $$$$$)
         seekBarPricing.setMax(5);
 
 
+        seekBarPricing.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-    }
+                StringBuilder strBuilder = new StringBuilder();
+                for(int i = 0; i < progress; i++){
+                    strBuilder.append("$");
+                }
+
+                textSeekBarPricing.setText(strBuilder);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        }
+
+
+
+
 
 }
