f16fc475337d48bfcb85119560101cdf8b700a60
John Nguyen-Tran <john.n.nguyentran@gmail.com>
2022-01-03 22:47:43 -0500

    Updated seekBar to update text to show range from one dollar sign to 5 dollar sign. Applied restriction to EditText for ZipCode input.

--- app/src/main/java/com/johnnguyentran/cuisinechooser/MainActivity.java
+++ app/src/main/java/com/johnnguyentran/cuisinechooser/MainActivity.java
@@ -3,7 +3,9 @@ package com.johnnguyentran.cuisinechooser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Get the editText from the .xml
+        EditText editTextZipCode = (EditText)findViewById(R.id.editTextZipCode);
+        editTextZipCode.setInputType(InputType.TYPE_CLASS_NUMBER);
+
 
         // Get the spinner from the .xml
         Spinner spinnerFoodType = (Spinner)findViewById(R.id.spinnerFoodTypeDropdown);
@@ -31,15 +39,15 @@ public class MainActivity extends AppCompatActivity {
         SeekBar seekBarPricing = (SeekBar)findViewById(R.id.seekBarPricing);
         TextView textSeekBarPricing = (TextView)findViewById(R.id.textSeekBarPricing);
         // Set maximum of 5 options ($ to $$$$$)
-        seekBarPricing.setMax(5);
+        seekBarPricing.setMax(4);
 
-
+        // Listener will update text based on slider
         seekBarPricing.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 StringBuilder strBuilder = new StringBuilder();
-                for(int i = 0; i < progress; i++){
+                for(int i = 0; i < progress + 1; i++){
                     strBuilder.append("$");
                 }
 
@@ -56,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
         }
 
